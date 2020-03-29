e24533e07786b3cf575bb9a4a785e6b1d0342a87
this4you <me_vlad@ukr.net>
2020-03-30 00:09:51 +0300

    Add JS number validation

--- LR4/task1/script.js
+++ LR4/task1/script.js
@@ -1,17 +1,49 @@
 let form = document.getElementById("setting-form");
 let image = document.getElementById("image");
+let elements = form.elements;
+let imgProperties = [
+    "width",
+    "height"
+];
+let validateSettings = {
+    "number": function(elem) {
+        elem.onkeydown = function(e) {
+            return e.key >= '0' && e.key <='9';
+        };
+    },
+    "string": function(elem) {
+
+    }
+};
+(function(){
+    for (let i = 0; i < elements.length - 1; i++) {
+        let elem = elements[i];
+        setValidation(elem);
+    }
+})();
+
 
 form.onsubmit = function (e) {
     e.preventDefault();
-    let elements = form.elements;
     for (let i = 0; i < elements.length - 1; i++) {
         let elem = elements[i];
-        if (elem.dataset.styleProperty === "height" || elem.dataset.styleProperty === "width") {
-            image[elem.dataset.styleProperty] = elem.value;
-        } else {
-            let dimension = elem.dataset.styleDimension || "";
-            image.style[elem.dataset.styleProperty] = elem.value  + dimension;  
-        }
+        setStyle(elem);
     }
+}
+
+function setStyle(elem) {
+    let styleProperty = elem.dataset.styleProperty;
+    if (imgProperties.indexOf(styleProperty) !== -1) {
+        image[styleProperty] = elem.value;
+    } else {
+        let dimension = elem.dataset.styleDimension || "";
+        image.style[styleProperty] = elem.value + dimension;
+    }
+}
+
+function setValidation(elem) {
+    let validation = elem.dataset.validate;
+    if (!validation) return;
+    validateSettings[validation](elem);
 }
 
