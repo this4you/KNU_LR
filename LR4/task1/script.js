c63fcdc9ce16aebcf9ddb920f9ca82a98dcdaca1
this4you <me_vlad@ukr.net>
2020-03-31 23:11:03 +0300

    Add empty and char validation

--- LR4/task1/script.js
+++ LR4/task1/script.js
@@ -1,21 +1,17 @@
 let form = document.getElementById("setting-form");
 let image = document.getElementById("image");
 let elements = form.elements;
-let imgProperties = [
-    "width",
-    "height"
-];
-let validateSettings = {
-    "number": function(elem) {
-        elem.onkeydown = function(e) {
-            return e.key >= '0' && e.key <='9';
-        };
-    },
-    "string": function(elem) {
 
+let validateSettings = {
+    "number": function (e) {
+        return (e.key >= '0' && e.key <= '9') || e.which === 8
+    },
+    "char": function (e) {
+        return /^[0-9a-zA-Z]+$/.test(e.key) && /[^\d]/g.test(e.key);
     }
 };
-(function(){
+
+(function () {
     for (let i = 0; i < elements.length - 1; i++) {
         let elem = elements[i];
         setValidation(elem);
@@ -27,13 +23,14 @@ form.onsubmit = function (e) {
     e.preventDefault();
     for (let i = 0; i < elements.length - 1; i++) {
         let elem = elements[i];
+        if (isEmptyValue(elem)) continue;
         setStyle(elem);
     }
 }
 
 function setStyle(elem) {
     let styleProperty = elem.dataset.styleProperty;
-    if (imgProperties.indexOf(styleProperty) !== -1) {
+    if (styleProperty in elem) {
         image[styleProperty] = elem.value;
     } else {
         let dimension = elem.dataset.styleDimension || "";
@@ -43,7 +40,17 @@ function setStyle(elem) {
 
 function setValidation(elem) {
     let validation = elem.dataset.validate;
-    if (!validation) return;
-    validateSettings[validation](elem);
+    if (!validation) return; // check consist in array
+    elem.onkeydown = validateSettings[validation];
+}
+
+function isEmptyValue(elem) {
+    if (elem.value == "") {
+        elem.classList.add("not-valid");
+        return true;
+    } else {
+        elem.classList.remove("not-valid");
+        return false;
+    }
 }
 
