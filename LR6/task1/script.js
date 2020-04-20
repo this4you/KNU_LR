ac20868cff31c9975559812ea9cd2bf75905a04a
this4you <me_vlad@ukr.net>
2020-04-20 11:33:25 +0300

    LR6 Task1

---
+++ LR6/task1/script.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function () {
+    $('#loginform').submit(function (e) {
+        e.preventDefault();
+        let $form = $(this);
+        if (!$form.valid()) return false;
+        let dataObject = {};
+        $form.serializeArray().forEach((el)=> {
+            dataObject[el.name] = el.value;
+        })
+        $.ajax({
+            type: "POST",
+            url: $form.attr('action'),
+            data: JSON.stringify(dataObject),
+            contentType: "application/json",
+            dataType: "json"
+        });
+    });
+    $("#loginform").validate({
+        rules: {
+            firstname: {
+                required: true,
+                minlength: 4,
+                maxlength: 16,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            messages: {
+                required: true,
+                maxlength: 250,
+            },
+        },
+        messages: {
+            firstname: {
+                required: "Это поле обязательно для заполнения",
+                minlength: "Имя должно быть минимум 4 символа",
+                maxlength: "Максимальное число символов - 16",
+            },
+            email: {
+                required: "Это поле обязательно для заполнения",
+                minlength: "Пароль должен быть минимум 6 символа",
+                maxlength: "Пароль должен быть максимум 16 символов",
+            },
+        }
+    });
+
+});
